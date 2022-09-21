660a353e56cb225b097d3ba40245224f7c9383ed
fwh <fwh@qq.com>
2022-09-21 21:23:13 +0800

    完成bbb

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -2,5 +2,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("bbb");
+        System.out.println("ccc");
     }
 }
